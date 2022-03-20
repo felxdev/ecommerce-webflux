ce0cfb85e69dff808bfe5d98acb1044ce95a436d
felix <felxdev@outlook.com>
2022-03-20 18:38:08 +0100

    Add api generated resources

---
+++ ecommerce-webflux-api-rest/src/main/java/ecommerce/webflux/service/app/api/rest/controllers/v1/RateApiController.java
@@ -0,0 +1,28 @@
+package ecommerce.webflux.service.app.api.rest.controllers.v1;
+
+import ecommerce.webflux.service.app.api.rest.dtos.v1.Rate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*",
+  methods = {
+      RequestMethod.POST
+  })
+public class RateApiController implements RateApi{
+
+  @Override
+  public Mono<ResponseEntity<Flux<Rate>>> addRate(Mono<Rate> rate,
+      ServerWebExchange exchange) {
+    return RateApi.super.addRate(rate, exchange);
+  }
+}
