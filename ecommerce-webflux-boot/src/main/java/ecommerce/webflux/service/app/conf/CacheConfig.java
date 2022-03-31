9bc50740a109797665bf333c55160e77ad425a67
felix <felxdev@outlook.com>
2022-03-31 02:32:13 +0200

    no message
    (cherry picked from commit 43739551d64421ec6c52722648dc8faf501bb3dd)

--- ecommerce-webflux-boot/src/main/java/ecommerce/webflux/service/app/conf/CacheConfig.java
+++ ecommerce-webflux-boot/src/main/java/ecommerce/webflux/service/app/conf/CacheConfig.java
@@ -1,10 +1,16 @@
 package ecommerce.webflux.service.app.conf;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
 import org.redisson.Redisson;
+import org.redisson.RedissonBatch;
+import org.redisson.RedissonReactive;
 import org.redisson.api.RedissonClient;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
+import org.redisson.reactive.RedissonBatchReactive;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
@@ -30,17 +36,22 @@ public class CacheConfig {
 
 
   @Bean
-  public CacheManager cacheManager(RedissonClient redissonClient) {
+  public CacheManager cacheManager(RedissonReactiveClient redissonClient) {
     Map<String, CacheConfig> config = new HashMap<>();
     config.put("rate", new CacheConfig());
     return new RedissonSpringCacheManager(redissonClient);
   }
 
   @Bean(destroyMethod = "shutdown")
-  public RedissonClient redisson() {
+  public RedissonReactiveClient redisson() {
+
     Config config = new Config();
+    config.setCodec(JsonJacksonCodec(ObjectMapper.));
     config.useSingleServer()
         .setAddress("redis://localhost:6379");
-    return Redisson.create(config);
+
+    RedissonClient redissonClient = Redisson.create(config);
+
+    return redissonClient.reactive();
   }
 }
